f0e406c749d4d5644911e63939920046bdb82fa3
Alan_Hu <hu_hanwen@163.com>
2025-03-29 00:17:28 +0800

    清除css默认样式&添加less支持

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,16 @@
+const CracoLessPlugin = require("craco-less");
 const path = require("path");
 
 module.exports = {
-	webpack: {
-		// 配置路径别名
-		alias: {
-			"@": path.resolve(__dirname, "src"),
-		},
-	},
+  webpack: {
+    // 配置路径别名
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+    },
+  ],
 };
